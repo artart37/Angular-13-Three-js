947fec83965011f0c18914159c5a774f525b698d
Aramayis Galichyan <aramayisgalichyan@gmail.com>
2022-02-21 04:04:02 +0400

    Fake service for the 3d models

---
+++ src/app/shared/services/boxes-service.ts
@@ -0,0 +1,69 @@
+/* tslint:disable */
+import { Injectable } from '@angular/core';
+import { HttpParams } from '@angular/common/http';
+
+import { Observable, of } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+import { BoxResponse } from '../components/shape-space/interfaces/shapes-interfaces';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class BoxService {
+  private boxes: BoxResponse[] = [
+    {
+      id: 0,
+      itemDescription: 'One',
+      color: '0X9D0707',
+      x: 1,
+      y: 1,
+      z: 1,
+    },
+    {
+      id: 1,
+      itemDescription: 'Two',
+      color: '0X07499D',
+      x: 2,
+      y: 1,
+      z: 4,
+    },
+    {
+      id: 3,
+      itemDescription: 'Three',
+      color: '0XA20BB4',
+      x: 1,
+      y: 3,
+      z: 5,
+    },
+  ];
+  constructor() {}
+
+  /**
+   * Get all boxes
+   */
+  getBoxes(): Observable<BoxResponse[]> {
+    return of(this.boxes).pipe(
+      catchError((error) => {
+        console.error(error, 'Error fetching boxes');
+        return of([]);
+      })
+    );
+  }
+
+  /**
+   * Delete the box by Id
+   * @param boxId Box Id to delete
+   * @return OK
+   */
+  deleteBoxes(boxId: number): Observable<BoxResponse | null> {
+    let __params = new HttpParams();
+    __params.append('boxId', boxId);
+
+    return of(this.boxes.find((box) => (box.id = boxId)) as BoxResponse).pipe(
+      catchError((error) => {
+        console.error(error, 'Error deleteing the box ');
+        return of(null);
+      })
+    );
+  }
+}
